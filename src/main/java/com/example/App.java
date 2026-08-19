2ebef7aa1bae0b0d1677f9b604f18b79c7eb7b98
udhayasurya01 <udhaya.surya@enochshipping.com>
2026-08-19 16:51:48 +0530

    Fix test mismatch

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,7 +3,7 @@ package com.example;
 public class App {
 
     public String getMessage() {
-        return "welcome";
+        return "hello";
     }
 
     public static void main(String[] args) {
